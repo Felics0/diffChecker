5230f8553512571fb0a371e0554b8108a6b01c48
Felice Pecchia <05felice.pecchia@gmail.com>
2024-04-08 17:43:42 +0200

    fixed

--- index.js
+++ index.js
@@ -17,20 +17,21 @@ async function main() {
     await pyodide.loadPackage("sympy")
     // let pythonCode = await (await fetch("main.py")).text();
     let pythonCode = `
-from sympy import limit, symbols, diff, simplify, S
+from sympy import limit, symbols, diff, simplify, sympify
 
 def calculator(fnction, x_0):
-    
     x = symbols('x')
-    derivative = diff(simplify(fnction), x)
+    fnction = simplify(sympify(fnction))
+    derivative = diff(fnction, x)
     fnction_limit = limit(fnction, x, x_0)
     right_fnction_limit = limit(fnction, x, x_0, "+")
     left_fnction_limit = limit(fnction, x, x_0, "-")
     right_limit = limit(derivative, x, x_0, "+")
     left_limit = limit(derivative, x, x_0, "-")
     fnction_y_0 = fnction.subs(x, x_0)
-    continuos = fnction_y_0 == right_fnction_limit and fnction_y_0 == left_fnction_limit
     derivative_y_0 = derivative.subs(x, x_0)
+    continuos = fnction_y_0 == right_fnction_limit and fnction_y_0 == left_fnction_limit
+
     return derivative, fnction_limit, right_limit, left_limit, continuos, derivative_y_0, fnction_y_0
 
 # Test the function
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from sympy import limit, symbols, diff, simplify, S
+from sympy import limit, symbols, diff, simplify, sympify
 
 def calculator(fnction, x_0):
-    
     x = symbols('x')
-    derivative = diff(simplify(fnction), x)
+    fnction = simplify(sympify(fnction))
+    derivative = diff(fnction, x)
     fnction_limit = limit(fnction, x, x_0)
     right_fnction_limit = limit(fnction, x, x_0, "+")
     left_fnction_limit = limit(fnction, x, x_0, "-")
     right_limit = limit(derivative, x, x_0, "+")
     left_limit = limit(derivative, x, x_0, "-")
+    
+    # Substituting value using subs for fnction_y_0
     fnction_y_0 = fnction.subs(x, x_0)
+    
+    # Substituting value using subs for derivative_y_0
+    derivative_y_0 = derivative.subs(x, x_0)
+    
     continuos = fnction_y_0 == right_fnction_limit and fnction_y_0 == left_fnction_limit
     
-    derivative_y_0 = derivative.subs(x, x_0)
     return derivative, fnction_limit, right_limit, left_limit, continuos, derivative_y_0, fnction_y_0
 
 # Test the function
-calculator(fnctionInput, dotInput)
+result = calculator(fnctionInput, dotInput)
+print(result)
